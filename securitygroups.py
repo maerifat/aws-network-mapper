0048d0c64cc0d9a179defd341e2a0df6f2c3b2c8
maerifat <76250027+maerifat@users.noreply.github.com>
2023-04-12 11:30:14 +0530

    added resource support of ec2 instances

--- securitygroups.py
+++ securitygroups.py
@@ -7,36 +7,101 @@ import html
 
 # set up the AWS sessions for dev and prod
 
-prod_session = boto3.Session(profile_name='prod')
-dev_session = boto3.Session(profile_name='dev')
+# prod_session = boto3.Session(profile_name='prod')
+security_session = boto3.Session(profile_name='sec')
+# staging_session= boto3.Session(profile_name='staging')
+# logging_session = boto3.Session(profile_name='logging')
+# infra_session = boto3.Session(profile_name='infra')
+# dev_session = boto3.Session(profile_name='dev')
+# sandbox_session = boto3.Session(profile_name='sandbox')
 
 # get the list of AWS regions
-
-
 #regions = prod_session.get_available_regions('ec2')
+regions = security_session.get_available_regions('ec2')
 #regions= ['af-south-1', 'ap-east-1', 'ap-northeast-1', 'ap-northeast-2', 'ap-northeast-3', 'ap-south-1', 'ap-south-2', 'ap-southeast-1', 'ap-southeast-2', 'ap-southeast-3', 'ap-southeast-4', 'ca-central-1', 'eu-central-1', 'eu-central-2', 'eu-north-1', 'eu-south-1', 'eu-south-2', 'eu-west-1', 'eu-west-2', 'eu-west-3', 'me-central-1', 'me-south-1', 'sa-east-1', 'us-east-1', 'us-east-2', 'us-west-1', 'us-west-2']
 #print(regions)
-regions=['ap-south-1']
 
 #regions= ['ap-south-1']
 # set up the HTML table
-table = PrettyTable(['S. No.', 'Account', 'Region', 'SecurityGroupId', 'Security Group Name', 'Protocol', 'Port Range', 'Source'], escape=False)
+table = PrettyTable(['S. No.', 'Account', 'Region', 'SecurityGroupId', 'Security Group Name', 'Resource Used','Protocol', 'Port Range', 'Source'], escape=False)
 table.align = "l"
 
 # initialize row count
 row_count = 0
 
+
+from botocore.exceptions import ClientError
+
+def find_ec2_instances(sg):
+    ec2 = session.client('ec2', region_name='ap-south-1')
+    try:
+        response = ec2.describe_instances(Filters=[
+                {'Name': 'instance.group-id', 'Values': [sg]}
+            ]) 
+        if not response['Reservations']:
+            instances = ""
+        else:
+            instances = []
+            for reservation in response['Reservations']:
+                for instance in reservation['Instances']:
+                    instances.append(instance['InstanceId'])
+        instance_ids = ' , '.join(instances)
+        return instance_ids
+    except ClientError as ex:
+        print(ex)
+
+
+
+
+
+
+def find_elbs(sg):
+    elb = session.client('elbv2', region_name='ap-south-1')
+    try:
+        response = elb.describe_load_balancers(Names=[], LoadBalancerArns=[], Marker='', PageSize=100)
+        if not response['LoadBalancers']:
+            elb_names = ""
+        else:
+            elb_names = []
+            for lb in response['LoadBalancers']:
+                for sg_id in lb['SecurityGroups']:
+                    if sg_id == sg:
+                        elb_names.append(lb['LoadBalancerName'])
+        elb_names = ' , '.join(elb_names)
+        return elb_names
+    except ClientError as ex:
+        print(ex)
+
+
+
+
+
+
+
+
+
+
+
+
+
 # iterate through the dev and prod sessions
 
-for session in [ dev_session,prod_session]:
+#for session in [ prod_session,dev_session , staging_session, infra_session,security_session,sandbox_session, logging_session ]:
+for session in [security_session]:
+#for session in [security_session]:
     # iterate through the regions
     for region in regions:
         try:
             ec2 = session.client('ec2', region_name=region)
+
             security_groups = ec2.describe_security_groups()['SecurityGroups']
             
             # iterate through the security groups and add to the table
             for sg in security_groups:
+                #print(sg)
+                resources= find_ec2_instances(str(sg['GroupId']))
+                print(f"this is my {resources}")
+                resource="abc"
                 for permission in sg['IpPermissions']:
                     protocol = permission['IpProtocol']
                     if protocol == '-1':
@@ -48,33 +113,30 @@ for session in [ dev_session,prod_session]:
                         portRange =   f"{permission.get('FromPort', '')}-{permission.get('FromPort', '')}" 
 
                     for ip_range in permission.get('IpRanges', []):
-                        # print(ip_range['CidrIp'])
-                        # print(permission.get('FromPort', ''))
-                        # print(permission.get('ToPort', ''))
-
-
 
                         row_count += 1
                         if (not ip_range['CidrIp'].startswith("sg-") and permission.get('FromPort', '') not in [443,80,8080] and not ip_range['CidrIp'].startswith("10." or "172." or "198.") and not ip_range['CidrIp'].endswith("/32") ):
 
-                            table.add_row([f"<font color='red'>{row_count}</font>", f"<font color='red'>{session.profile_name}</font>", f"<font color='red'>{region}</font>", f"<font color='red'>{sg['GroupId']}</font>", f"<font color='red'>{sg['GroupName']}</font>", f"<font color='red'>{protocol}</font>", f"<font color='red'>{portRange}</font>", f"<font color='red'>{ip_range['CidrIp']}</font>"])
+                            table.add_row([f"<font color='red'>{row_count}</font>", f"<font color='red'>{session.profile_name}</font>", f"<font color='red'>{region}</font>", f"<font color='red'>{sg['GroupId']}</font>", f"<font color='red'>{sg['GroupName']}</font>",f"<font color='red'>{resources}</font>", f"<font color='red'>{protocol}</font>", f"<font color='red'>{portRange}</font>", f"<font color='red'>{ip_range['CidrIp']}</font>"])
                             print(permission.get('FromPort', ''))
                             
                         else:
-                            table.add_row([row_count, session.profile_name, region, sg['GroupId'], sg['GroupName'], protocol, portRange, ip_range['CidrIp']])
+                            table.add_row([row_count, session.profile_name, region, sg['GroupId'], sg['GroupName'],resources, protocol, portRange, ip_range['CidrIp']])
                     for ipv6_range in permission.get('Ipv6Ranges', []):
                         row_count += 1
                         if (not ip_range['CidrIp'].startswith("sg-") and permission.get('FromPort', '') not in [80,443,8080] and not ip_range['CidrIp'].startswith("10." or "172." or "198.") and not ip_range['CidrIp'].endswith("/32") ):
                             
-                            table.add_row([f"<font color='red'>{row_count}</font>", f"<font color='red'>{session.profile_name}</font>", f"<font color='red'>{region}</font>", f"<font color='red'>{sg['GroupId']}</font>", f"<font color='red'>{sg['GroupName']}</font>", f"<font color='red'>{protocol}</font>", f"<font color='red'>{portRange}</font>", f"<font color='red'>{ip_range['CidrIp']}</font>"])
+                            table.add_row([f"<font color='red'>{row_count}</font>", f"<font color='red'>{session.profile_name}</font>", f"<font color='red'>{region}</font>", f"<font color='red'>{sg['GroupId']}</font>", f"<font color='red'>{sg['GroupName']}</font>", f"<font color='red'>{resources}</font>", f"<font color='red'>{protocol}</font>", f"<font color='red'>{portRange}</font>", f"<font color='red'>{ip_range['CidrIp']}</font>"])
                         else:
-                            table.add_row([row_count, session.profile_name, region, sg['GroupId'], sg['GroupName'], protocol, portRange, ipv6_range['CidrIpv6']])
+                            table.add_row([row_count, session.profile_name, region, sg['GroupId'], sg['GroupName'], resources, protocol, portRange, ipv6_range['CidrIpv6']])
                     for group_pair in permission.get('UserIdGroupPairs', []):
                         print(group_pair)
                         row_count += 1
-                        table.add_row([row_count, session.profile_name, region, sg['GroupId'], sg['GroupName'], protocol, portRange, group_pair['GroupId']])
-        except:
-            print({region})
+                        table.add_row([row_count, session.profile_name, region, sg['GroupId'], sg['GroupName'],resources, protocol, portRange, group_pair['GroupId']])
+        except ClientError as e:
+            print(e)
+        # except:
+        #     print(region)
 
 # get the HTML code for the table
 html_string = html.unescape(table.get_html_string(attributes={"id": "network-table"}))
